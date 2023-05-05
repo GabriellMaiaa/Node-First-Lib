3e4f3bd69198794b8522780c4bdeff841f65c4e5
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-05 10:44:38 -0300

    adding if

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+import fs from 'fs'
 import chalk from 'chalk';
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo nesse diretório'))
+}
+
+function pegaArquivo(caminhoArquivo) {
+    const enconding = 'utf-8'
+    fs.readFile(caminhoArquivo, enconding, (erro, texto) => {
+        if(erro) {
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/')
