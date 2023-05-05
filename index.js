9237c8babd870008e99dbc5c9035dd18ef7d6fbb
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-05 11:37:25 -0300

    promises com then e catch

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo nesse diretório'))
 }
 
-function pegaArquivo(caminhoArquivo) {
-    const enconding = 'utf-8'
-    fs.readFile(caminhoArquivo, enconding, (erro, texto) => {
-        if(erro) {
-            trataErro(erro)
-        }
-        console.log(chalk.green(texto))
-    })
+function pegaArquivo(caminhoArquivo) {//vAMOS FAZER UMA FUNÇÃO ASSÍNCRONA
+    const enconding = 'utf-8';
+    fs.promises.readFile(caminhoArquivo, enconding)
+        .then((texto) => console.log(chalk.blue(texto)))
+        .catch((erro) => trataErro(erro))
 }
 
-pegaArquivo('./arquivos/')
+// function pegaArquivo(caminhoArquivo) {
+//     const enconding = 'utf-8'
+//     fs.readFile(caminhoArquivo, enconding, (erro, texto) => {
+//         if(erro) {
+//             trataErro(erro)
+//         }
+//         console.log(chalk.green(texto))
+//     })
+// }
+
+pegaArquivo('./arquivos/texto.md')
