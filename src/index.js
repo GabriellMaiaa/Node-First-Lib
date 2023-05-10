6c0088f73dea0295cdc0fcd3c083e822fa5656e3
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-10 11:51:36 -0300

    create a cli

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import fs from 'fs'
 import chalk from 'chalk';
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
 function extraiLinks (texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
 
     const capturas = [...texto.matchAll(regex)]
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    console.log(resultados)
+    return resultados;
 }
 
-
-
-extraiLinks(textoTeste)
 
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo nesse diretório'))
@@ -25,7 +21,7 @@ async function pegaArquivo(caminhoArquivo) {//Ele vai tentar, e se estiver corre
     try {
         const enconding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoArquivo, enconding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(chalk.green(texto)))
     }
     catch(erro) {
         trataErro(erro)
@@ -51,7 +47,7 @@ async function pegaArquivo(caminhoArquivo) {//Ele vai tentar, e se estiver corre
 //     })
 // }
 
-// pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/texto.md')
 // pegaArquivo('./arquivos/')
 
 // \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
