afd778cfa421567d4aed10b44f6cd55ee3e6ebe9
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-11 11:07:08 -0300

    adding read in diretory and files

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,26 @@
 import chalk from "chalk";
 import pegaArquivo from "./index.js";
+import fs from 'fs'
 
 const caminho = process.argv;
 console.log(caminho[2])//Pega o 3 argumento da chamada no terminal
 
-pegaArquivo(caminho[2])//Elee pega o 3 argumento passado no comando do terminal que está aqui abaixo
+// pegaArquivo(caminho[2])//Elee pega o 3 argumento passado no comando do terminal que está aqui abaixo
 
 // node src/cli.js ./arquivos/texto.md
 
-async function processaTexto (caminho) {
-    const resultado = await pegaArquivo(caminho[2])//Pega o 3 elemento na busca
-    console.log(chalk.blue('lista de links'), resultado)
+async function processaTexto (argumentos) {
+    const caminho = argumentos[2];
+
+    if(fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho)//Pega o 3 elemento na busca
+        console.log(chalk.blue('lista de links'), resultado)
+  } 
+    else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+        console.log(arquivos)
+  }
+
 }
 
 processaTexto(caminho)
